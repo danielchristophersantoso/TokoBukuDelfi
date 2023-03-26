c8a1c18474439c248b03d28d65425246a7ac593f
Daniel Christopher <daniel.cs4real@gmail.com>
2023-03-26 16:24:37 +0700

    Hello World

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing");
-        System.out.println("Delvin");
-        System.out.println("asdfgh");
-        System.out.println("Delvin 2");
-        System.out.println("Daniel");
+        System.out.println("Hello World!");
     }
 }
