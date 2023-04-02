eab19e944bca3fa70278e18316cb4c964b1a30d1
Interblues88 <delvin.setiamin.08@gmail.com>
2023-04-02 18:16:41 +0700

    awal ngoding tambah pelanggan baru

---
+++ src/HapusKoleksi.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HapusKoleksi extends JFrame implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
