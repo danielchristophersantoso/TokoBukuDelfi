9a408f682c8d1091aa0dfd79924b475e6cf00f14
Interblues88 <delvin.setiamin.08@gmail.com>
2023-04-03 11:22:57 +0700

    nambah

--- src/HapusKoleksi.java
+++ src/HapusKoleksi.java
@@ -3,6 +3,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class HapusKoleksi extends JFrame implements ActionListener {
+    private JLabel headerLabel= new JLabel("Hapus Koleksi");
+    private JLabel namaKoleksiLabel = new JLabel("Nama Koleksi");
+    private JTextField namaKoleksiField = new JTextField();
+    private JButton hapusKoleksiBtn = new JButton("Hapus Koleksi");
+    public HapusKoleksi(){
+        super("Hapus Koleksi");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setBounds(300, 90, 800, 490);
+        setVisible(true);
+        setResizable(false);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
