8bc7cc38605bbc2bb9ffe51797ce49eca48716a0
Daniel Christopher <daniel.cs4real@gmail.com>
2023-04-24 01:27:07 +0700

    Update Menu Utama

--- src/MenuUtama.java
+++ src/MenuUtama.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 public class MenuUtama extends JFrame implements ActionListener {
     private JMenuBar menuBar = new JMenuBar();
     private JMenuItem keluarMenuItem = new JMenuItem("Keluar");
+    private JLabel titleLabel = new JLabel("Menu Utama");
+    private JPanel panel = new JPanel();
     private String[] btnLabels = new String[] {
             "Tambah Buku Baru", "Hapus Buku ", "Tambah Koleksi Baru",
             "Hapus Koleksi", "Tampilkan Riwayat Transaksi",
@@ -13,19 +15,34 @@ public class MenuUtama extends JFrame implements ActionListener {
     };
     private JButton[] buttons = new JButton[]{};
     public MenuUtama() {
-        super("Menu Utama");
+        this.setTitle("Menu Utama");
         menuBar.add(keluarMenuItem);
         keluarMenuItem.addActionListener(this);
         setJMenuBar(menuBar);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 40));
+        titleLabel.setHorizontalAlignment(JLabel.CENTER);
+        titleLabel.setBounds(0, 35, 880, 40);
+        add(titleLabel, BorderLayout.NORTH);
+        panel.setBounds(290, 100, 300, 300);
+        panel.setBackground(Color.gray);
+        panel.setLayout(new GridLayout(8, 1));
 
-        setLocationRelativeTo(null);
-        setLayout(null);
-        setSize(880, 495);
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().setBackground(Color.LIGHT_GRAY);
-        setIconImage((new ImageIcon(this.getClass().getResource("icon.png"))).getImage());
-        setVisible(true);
+        for (String l: btnLabels){
+            JButton btn = new JButton(l);
+            btn.addActionListener(this);
+            btn.setFocusable(false);
+            panel.add(btn);
+        }
+
+        this.add(panel);
+        this.setFocusable(false);
+        this.setLayout(null);
+        this.setSize(880, 495);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setBackground(Color.LIGHT_GRAY);
+        this.setIconImage((new ImageIcon(this.getClass().getResource("icon.png"))).getImage());
+        this.setVisible(true);
     }
 
     public static void main(String[] args) {
